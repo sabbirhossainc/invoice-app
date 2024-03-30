be984237ef1d3a0c6bba2da72f65d5c3da1cf493
sabbirhossainc <36562655+sabbirhossainc@users.noreply.github.com>
2024-03-30 19:50:24 +0600

    Add customerInfoComp

--- src/forms/customerInfo.jsx
+++ src/forms/customerInfo.jsx
@@ -1,76 +1,67 @@
-
 const customerInfo = () => {
   return (
     <div className="pt-8 mx-auto max-w-3xl">
-          <h2 className="mb-6 pb-1 text-xl font-medium text-gray-900 border-b-2 border-primary-500">
-            Customer Information
-          </h2>
-          <form action="#">
-            <div className="flex flex-col gap-4 p-4 border border-gray-300 rounded-md">
-              <div className="">
-                <label
-                  className="block mb-2 text-sm font-light text-gray-900"
-                >
-                  First Name <span className="text-red-600">*</span>
-                </label>
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500  block w-full p-2"
-                  placeholder="Type first name"
-                  required="true"
-                />
-              </div>
-              <div className="">
-                <label
-                  className="block mb-2 text-sm font-light text-gray-900"
-                >
-                  Last Name <span className="text-red-600">*</span>
-                </label>
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 block w-full p-2"
-                  placeholder="Type last name"
-                  required="true"
-                />
-              </div>
-              <div className="">
-                <label
-                  className="block mb-2 text-sm font-light text-gray-900"
-                >
-                  Email <span className="text-red-600">*</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 block w-full p-2"
-                  placeholder="Type email"
-                  required="true"
-                />
-              </div>
-              <div className="">
-                <label
-                  className="block mb-2 text-sm font-light text-gray-900"
-                >
-                  Phone <span className="text-red-600">*</span>
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone-input"
-                  className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 block w-full p-2"
-                  placeholder="Type phone number"
-                  required="true"
-                />
-              </div>
-            </div>
-          </form>
+      <h2 className="mb-6 pb-1 text-xl font-medium text-gray-900 border-b-2 border-primary-500">
+        Customer Information
+      </h2>
+      <form action="#">
+        <div className="flex flex-col gap-4 p-4 border border-gray-300 rounded-md">
+          <div className="">
+            <label className="block mb-2 text-sm font-light text-gray-900">
+              First Name <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500  block w-full p-2"
+              placeholder="Type first name"
+              required="true"
+            />
+          </div>
+          <div className="">
+            <label className="block mb-2 text-sm font-light text-gray-900">
+              Last Name <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 block w-full p-2"
+              placeholder="Type last name"
+              required="true"
+            />
+          </div>
+          <div className="">
+            <label className="block mb-2 text-sm font-light text-gray-900">
+              Email <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 block w-full p-2"
+              placeholder="Type email"
+              required="true"
+            />
+          </div>
+          <div className="">
+            <label className="block mb-2 text-sm font-light text-gray-900">
+              Phone <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              name="phone"
+              id="phone-input"
+              className="border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 block w-full p-2"
+              placeholder="Type phone number"
+              required="true"
+            />
+          </div>
         </div>
-  )
-}
+      </form>
+    </div>
+  );
+};
 
-export default customerInfo
+export default customerInfo;
